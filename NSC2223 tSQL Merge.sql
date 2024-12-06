332fb0e1ddb457b70adf2d46c47b23cdfb2134d4
GarnesDoug <91558249+GarnesDoug@users.noreply.github.com>
2024-12-06 16:02:22 -0600

    Update NSC2223 tSQL Merge.sql
    Updated to account for new column name changes because it was getting confusing.

--- NSC2223 tSQL Merge.sql
+++ NSC2223 tSQL Merge.sql
@@ -1,15 +1,11 @@
 
-
-
-
-
 SELECT  
        n.RecordType [Record Type]
 	 , n.OPEID [Institution Code (OPEID)]
 	 , n.AwardYear [Award Year]
 	 , n.StudentSSN [Student Social Security Number]
 	 , n.FirstName [Student First Name]
-	 , n.MiddleName [Student Middle Name]
+	 , ISNULL(n.MiddleName,'') [Student Middle Name]
 	 , n.LastName [Student Last Name]
 	 , n.dob [Student Date of Birth]
 	 , n.CIPCode [CIP Code]
@@ -17,34 +13,37 @@ SELECT
 	 , n.PubPrgLen [Published Length of Program]
 	 , n.PubLengthProMsrm [Published Length of Program Measurement]
 	 , n.WeeksinTIV [Weeks in Title IV Academic Year]
-	 , n.CTP [Comprehensive Transition and Postsecondary (CTP) Program Indicator]
-	 , n.AppPrisEdu [Approved Prison Education Program Indicator]
+	 , ISNULL(n.CTP,'') [Comprehensive Transition and Postsecondary (CTP) Program Indicator]
+	 , ISNULL(n.AppPrisEdu,'') [Approved Prison Education Program Indicator]
 	 , n.DateCompWD [Date Student Completed or Withdrew from Program]
-	 , f.TOTALPRIVATELOANS [Total Amount Student Received in Private Education Loans During Student's Entire Enrollment in the Program]
-	 , f.TOTALINSTDEBT [Total Amount of Institutional Debt During Student's Entire Enrollment in the Program]
-	 , f.TOTALTUITIONANDFEESASSESSED [Total Amount of Tuition & Fees Assessed During Student's Entire Enrollment in the Program]
-	 , f.TOTALBOOKSSUPPLIESEQUIPMENT [Total Amount of Allowance for books, supplies, and equipment included in the student's title IV]
-	 , f.TOTALGRANTSANDSCHOLARSHIPS [Total Amount of Grants and Scholarships the student received During Student's Entire Enrollment in the Program]
-	 , n.EnrlBegDate [Program Enrollment Begin Date]
-	 , n.StuEnrlStatus [Student's Enrollment Status as of the 1st Day of Enrollment in the Program]
-	 , n.ProgStatus [Program Attendance Status During Award Year]
-	 , n.ProgStatusDate [Program Attendance Status Date During Award Year]
-	 , f.ANNUALCOA [Annual Cost of Attendance (COA)]
-	 , f.ANNUALTUITIONANDFEESASSESSED [Tuition and Fees Amount for Award Year being Reported]
+	 , ISNULL(f.TOTALPRIVATELOANS,0) [Total Amount Student Received in Private Education Loans During Student's Entire Enrollment in the Program]
+	 , ISNULL(f.TOTALINSTDEBT, 0) [Total Amount of Institutional Debt During Student's Entire Enrollment in the Program]
+	 , ISNULL(f.TOTALTUITIONANDFEESASSESSED, 0) [Total Amount of Tuition & Fees Assessed During Student's Entire Enrollment in the Program]
+	 , ISNULL(f.TOTALBOOKSSUPPLIESEQUIPMENT, 0) [Total Amount of Allowance for books, supplies, and equipment included in the student's title IV]
+	 , ISNULL(f.TOTALGRANTSANDSCHOLARSHIPS, 0) [Total Amount of Grants and Scholarships the student received During Student's Entire Enrollment in the Program]
+	 , ISNULL(n.EnrlBegDate,'') [Program Enrollment Begin Date]
+	 , ISNULL(n.StuEnrlStatus,'') [Student's Enrollment Status as of the 1st Day of Enrollment in the Program]
+	 , ISNULL(n.ProgStatus,'') [Program Attendance Status During Award Year]
+	 , ISNULL(n.ProgStatusDate,'') [Program Attendance Status Date During Award Year]
+	 , ISNULL(f.ANNUALCOA, 0) [Annual Cost of Attendance (COA)]
+	 , ISNULL(f.ANNUALTUITIONANDFEESASSESSED, 0) [Tuition and Fees Amount for Award Year being Reported]
 	 , 'OS' [Residency Tuition Status by State or District]
-	 , f.ANNUALBOOKSSUPPLIESEQUIPMENT [Allowance for Books, Supplies, and Equipment]
-	 , f.ANNUALHOUSINGANDFOOD [Allowance for Housing and Food]
-	 , f.ANNUALINSTGRANTSANDSCHOLARSHIPS [Institutional Grants and Scholarships]
-	 , f.ANNUALOTHERSTATETRIBALPRIVATEGRANTSANDSCHOLARSHIPS [Other State, Tribal, or Private Grants]
-	 , f.ANNUALPRIVATELOANAMOUNT [Private Loans Amount]
-	 , f.INVALIDFLAG [Invalid Flag]
+	 , ISNULL(f.ANNUALBOOKSSUPPLIESEQUIPMENT, 0) [Allowance for Books, Supplies, and Equipment]
+	 , ISNULL(f.ANNUALHOUSINGANDFOOD, 0) [Allowance for Housing and Food]
+	 , ISNULL(f.ANNUALINSTGRANTSANDSCHOLARSHIPS, 0) [Institutional Grants and Scholarships]
+	 , ISNULL(f.ANNUALOTHERSTATETRIBALPRIVATEGRANTSANDSCHOLARSHIPS, 0) [Other State, Tribal, or Private Grants]
+	 , ISNULL(f.ANNUALPRIVATELOANAMOUNT,0) [Private Loans Amount]
+	 , CASE WHEN f.INVALIDFLAG = 'T' THEN 'T'
+	        WHEN f.INVALIDFLAG= 'N' AND n.ProgStatus = 'W' THEN 'R'
+			ELSE 'N' END [Invalid Flag]
 	 , 'N' [Gainful Employment Program Flag]
 FROM nsc.nsc2223 n
 	LEFT JOIN nsc.FVT f ON
-	f.STUDENTID = n.StudentID
+	f.NSCSTUDENTID = n.StudentID
 	AND f.NSCAIDYEAR = n.AwardYear
-	AND f.CIPCODE = n.CIPCode
-    AND f.RECORDTYPE = n.RecordType
+	AND f.NSCCIPCODE = n.CIPCode
+    AND f.NSCRECORDTYPE = n.RecordType
+    AND f.NSCCREDENTIALCODE = n.CredentialLevel
 WHERE n.StudentID IS NOT NULL
 UNION ALL 
 SELECT  
@@ -53,7 +52,7 @@ SELECT
 	 , n.AwardYear [Award Year]
 	 , n.StudentSSN [Student Social Security Number]
 	 , n.FirstName [Student First Name]
-	 , n.MiddleName [Student Middle Name]
+	 , ISNULL(n.MiddleName,'') [Student Middle Name]
 	 , n.LastName [Student Last Name]
 	 , n.dob [Student Date of Birth]
 	 , n.CIPCode [CIP Code]
@@ -61,32 +60,35 @@ SELECT
 	 , n.PubPrgLen [Published Length of Program]
 	 , n.PubLengthProMsrm [Published Length of Program Measurement]
 	 , n.WeeksinTIV [Weeks in Title IV Academic Year]
-	 , n.CTP [Comprehensive Transition and Postsecondary (CTP) Program Indicator]
-	 , n.AppPrisEdu [Approved Prison Education Program Indicator]
+	 , ISNULL(n.CTP,'') [Comprehensive Transition and Postsecondary (CTP) Program Indicator]
+	 , ISNULL(n.AppPrisEdu,'') [Approved Prison Education Program Indicator]
 	 , n.DateCompWD [Date Student Completed or Withdrew from Program]
-	 , f.TOTALPRIVATELOANS [Total Amount Student Received in Private Education Loans During Student's Entire Enrollment in the Program]
-	 , f.TOTALINSTDEBT [Total Amount of Institutional Debt During Student's Entire Enrollment in the Program]
-	 , f.TOTALTUITIONANDFEESASSESSED [Total Amount of Tuition & Fees Assessed During Student's Entire Enrollment in the Program]
-	 , f.TOTALBOOKSSUPPLIESEQUIPMENT [Total Amount of Allowance for books, supplies, and equipment included in the student's title IV]
-	 , f.TOTALGRANTSANDSCHOLARSHIPS [Total Amount of Grants and Scholarships the student received During Student's Entire Enrollment in the Program]
-	 , n.EnrlBegDate [Program Enrollment Begin Date]
-	 , n.StuEnrlStatus [Student's Enrollment Status as of the 1st Day of Enrollment in the Program]
-	 , n.ProgStatus [Program Attendance Status During Award Year]
-	 , n.ProgStatusDate [Program Attendance Status Date During Award Year]
-	 , f.ANNUALCOA [Annual Cost of Attendance (COA)]
-	 , f.ANNUALTUITIONANDFEESASSESSED [Tuition and Fees Amount for Award Year being Reported]
+     , ISNULL(f.TOTALPRIVATELOANS,0) [Total Amount Student Received in Private Education Loans During Student's Entire Enrollment in the Program]
+	 , ISNULL(f.TOTALINSTDEBT, 0) [Total Amount of Institutional Debt During Student's Entire Enrollment in the Program]
+	 , ISNULL(f.TOTALTUITIONANDFEESASSESSED, 0) [Total Amount of Tuition & Fees Assessed During Student's Entire Enrollment in the Program]
+	 , ISNULL(f.TOTALBOOKSSUPPLIESEQUIPMENT, 0) [Total Amount of Allowance for books, supplies, and equipment included in the student's title IV]
+	 , ISNULL(f.TOTALGRANTSANDSCHOLARSHIPS, 0) [Total Amount of Grants and Scholarships the student received During Student's Entire Enrollment in the Program]
+	 , ISNULL(n.EnrlBegDate,'') [Program Enrollment Begin Date]
+	 , ISNULL(n.StuEnrlStatus,'') [Student's Enrollment Status as of the 1st Day of Enrollment in the Program]
+	 , ISNULL(n.ProgStatus,'') [Program Attendance Status During Award Year]
+	 , ISNULL(n.ProgStatusDate,'') [Program Attendance Status Date During Award Year]
+	 , ISNULL(f.ANNUALCOA, 0) [Annual Cost of Attendance (COA)]
+	 , ISNULL(f.ANNUALTUITIONANDFEESASSESSED, 0) [Tuition and Fees Amount for Award Year being Reported]
 	 , 'OS' [Residency Tuition Status by State or District]
-	 , f.ANNUALBOOKSSUPPLIESEQUIPMENT [Allowance for Books, Supplies, and Equipment]
-	 , f.ANNUALHOUSINGANDFOOD [Allowance for Housing and Food]
-	 , f.ANNUALINSTGRANTSANDSCHOLARSHIPS [Institutional Grants and Scholarships]
-	 , f.ANNUALOTHERSTATETRIBALPRIVATEGRANTSANDSCHOLARSHIPS [Other State, Tribal, or Private Grants]
-	 , f.ANNUALPRIVATELOANAMOUNT [Private Loans Amount]
-	 , f.INVALIDFLAG [Invalid Flag]
+	 , ISNULL(f.ANNUALBOOKSSUPPLIESEQUIPMENT, 0) [Allowance for Books, Supplies, and Equipment]
+	 , ISNULL(f.ANNUALHOUSINGANDFOOD, 0) [Allowance for Housing and Food]
+	 , ISNULL(f.ANNUALINSTGRANTSANDSCHOLARSHIPS, 0) [Institutional Grants and Scholarships]
+	 , ISNULL(f.ANNUALOTHERSTATETRIBALPRIVATEGRANTSANDSCHOLARSHIPS, 0) [Other State, Tribal, or Private Grants]
+	 , ISNULL(f.ANNUALPRIVATELOANAMOUNT,0) [Private Loans Amount]
+	 , CASE WHEN f.INVALIDFLAG = 'T' THEN 'T'
+	        WHEN f.INVALIDFLAG= 'N' AND n.ProgStatus = 'W' THEN 'R'
+			ELSE 'N' END [Invalid Flag]
 	 , 'N' [Gainful Employment Program Flag]
 FROM nsc.nsc2223 n
     LEFT JOIN nsc.FVT f ON
-	    f.SSN = n.StudentSSN
+	    f.NSCSSN = n.StudentSSN
 	AND f.NSCAIDYEAR = n.AwardYear
-	AND f.CIPCODE = n.CIPCode
-	AND f.RECORDTYPE = n.RecordType
+	AND f.NSCCIPCODE = n.CIPCode
+	AND f.NSCRECORDTYPE = n.RecordType
+    AND f.NSCCREDENTIALCODE = n.CredentialLevel
 WHERE n.StudentID IS NULL
